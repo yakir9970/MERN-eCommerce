efb976b6e1a116d1da18a8327f4ff9c1aac49b46
yakir9970 <yakirafeka@gmail.com>
2022-10-09 16:28:50 +0300

    Bootstrap, Header, Footer

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart me-1"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user me-1"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
