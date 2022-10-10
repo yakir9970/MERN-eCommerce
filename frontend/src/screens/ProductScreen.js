962d589cdba980c1bc3eae9052543744cb2ce953
yakir9970 <yakirafeka@gmail.com>
2022-10-10 20:14:41 +0300

    Product screen style

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,86 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+import "./ProductScreen.css";
 
-const ProductScreen = () => {
-  return <div>product</div>;
+const ProductScreen = (props) => {
+  const params = useParams();
+  const product = products.find((p) => p._id === params.pid);
+  return (
+    <React.Fragment>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>${product.price}</h2>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col
+                    style={{
+                      color: `${product.countInStock > 0 ? "green" : "red"}`,
+                    }}
+                    className="col-stock"
+                  >
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className="d-grid gap-2">
+                  <Button
+                    className="add-to-cart"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8} className="mt-5">
+          <h2 className="h2-desc">Description</h2>
+          <ListGroup.Item>
+            <h4>{product.description}</h4>
+          </ListGroup.Item>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 };
 
 export default ProductScreen;
