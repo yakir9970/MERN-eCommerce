a336cd5c85b130afce570acf1405eb17b22619dc
yakir9970 <yakirafeka@gmail.com>
2022-10-11 22:31:21 +0300

    Adding Redux to home and product page

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstans.js";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const listProductDetails = (pid) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${pid}`);
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { listProductDetails } from "../actions/productActions";
 import "./ProductScreen.css";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const ProductScreen = (props) => {
   const params = useParams();
-
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${params.pid}`);
-      setProduct(data);
-    };
-    fetchProduct();
-  }, [params]);
+    dispatch(listProductDetails(params.pid));
+  }, [dispatch, params]);
 
   return (
     <React.Fragment>
@@ -27,65 +27,71 @@ const ProductScreen = (props) => {
       >
         Go Back
       </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h2>${product.price}</h2>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={4}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col style={{ fontWeight: "bold" }}>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col style={{ fontWeight: "bold" }}>Status:</Col>
-                  <Col
-                    style={{
-                      fontWeight: "bold",
-                      color: `${product.countInStock > 0 ? "green" : "red"}`,
-                    }}
-                    className="col-stock"
-                  >
-                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
               <ListGroup.Item>
-                <div className="d-grid gap-2">
-                  <Button
-                    className="add-to-cart"
-                    disabled={product.countInStock === 0}
-                  >
-                    Add To Cart
-                  </Button>
-                </div>
+                <h2>${product.price}</h2>
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={4}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col style={{ fontWeight: "bold" }}>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col style={{ fontWeight: "bold" }}>Status:</Col>
+                    <Col
+                      style={{
+                        fontWeight: "bold",
+                        color: `${product.countInStock > 0 ? "green" : "red"}`,
+                      }}
+                      className="col-stock"
+                    >
+                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <div className="d-grid gap-2">
+                    <Button
+                      className="add-to-cart"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
       <Row>
         <Col md={8} className="mt-5">
           <h2 className="h2-desc">Description</h2>
