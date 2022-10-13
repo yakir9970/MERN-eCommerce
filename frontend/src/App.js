a13355207e90c2463c013cba492e357874b7e38e
yakir9970 <yakirafeka@gmail.com>
2022-10-13 17:27:58 +0300

    Added user register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
             <Route path="/product/:pid" element={<ProductScreen />} />
             <Route path="/cart">
               <Route path=":pid" element={<CartScreen />} />
