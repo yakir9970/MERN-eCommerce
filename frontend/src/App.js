d962b934d5373122f050b621cd313ea824bfd6aa
yakir9970 <yakirafeka@gmail.com>
2022-10-16 12:01:08 +0300

    Shipping screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/shipping" element={<ShippingScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -37,7 +37,7 @@ const CartScreen = (props) => {
   };
 
   const checkoutHandler = (id) => {
-    navigate("/login?redirect=shipping");
+    navigate("/login/?redirect=/shipping");
   };
 
   return (
