da824b70952dbc19368d4d1edc5c014917a484a1
yakir9970 <yakirafeka@gmail.com>
2022-10-19 14:32:06 +0300

    update order to paid route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import protect from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:oid").get(protect, getOrderById);
+router.route("/:oid/pay").put(protect, updateOrderToPaid);
 
 export default router;
