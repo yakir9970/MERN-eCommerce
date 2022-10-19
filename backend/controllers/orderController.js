ac9d2ad934c45401271415a34ccc91eacf37fce6
yakir9970 <yakirafeka@gmail.com>
2022-10-19 13:40:25 +0300

    Get order by id route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,4 +37,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc    Get order by id
+// @route   GET /api/orders/:oid
+// @access  private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.oid).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order Not Found!");
+  }
+});
+
+export { addOrderItems, getOrderById };
