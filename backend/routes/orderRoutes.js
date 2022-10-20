6ae0e742e475ab77ced82de4c8177051b84618dd
yakir9970 <yakirafeka@gmail.com>
2022-10-20 11:37:38 +0300

    Paypal added and my orders route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,9 +4,11 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from "../controllers/orderController.js";
 import protect from "../middleware/authMiddleware.js";
 
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/").post(protect, addOrderItems);
 router.route("/:oid").get(protect, getOrderById);
 router.route("/:oid/pay").put(protect, updateOrderToPaid);
--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -76,3 +79,43 @@ export const getOrderDetails = (oid) => async (dispatch, getState) => {
     });
   }
 };
+
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload: message,
+      });
+    }
+  };
