a6798d8b463a3c4787dd5527b402d3be298d3965
yakir9970 <yakirafeka@gmail.com>
2022-10-21 21:46:22 +0300

    Admin get and update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,6 +128,44 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get user by id
+// @route   GET /api/users/:uid
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.uid).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User Not Found!");
+  }
+});
+
+// @desc    update user
+// @route   PUT /api/users/:uid
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.uid);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found!");
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -135,4 +173,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
